fe240703568a5b29a92ac09626150e14413438bb
nakaly <ryosuke.nakamura@gmail.com>
2016-11-25 15:46:12 +0900

    Add new filters

--- server.py
+++ server.py
@@ -21,4 +21,16 @@ def seurat():
 	f.write('original')
 	f.close()
 
+@route('/style/hokusai')
+def seurat():
+	f = open('/home/ec2-user/filter-mode','w')
+	f.write('hokusai')
+	f.close()
+
+@route('/style/kandinsky')
+def seurat():
+	f = open('/home/ec2-user/filter-mode','w')
+	f.write('kandinsky')
+	f.close()
+
 run(host='0.0.0.0', port=8080, debug=True)
