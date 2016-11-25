c05cbd41e05fc291bb251d76637a0d04de599c4e
nakaly <ryosuke.nakamura@gmail.com>
2016-11-25 14:44:14 +0900

    Add api server

---
+++ server.py
@@ -0,0 +1,24 @@
+from bottle import route, run
+
+@route('/hello')
+def hello():
+	return "Hello World!"
+@route('/style/seurat')
+def seurat():
+	f = open('/home/ec2-user/filter-mode','w')
+	f.write('seurat')
+	f.close()
+
+@route('/style/composition')
+def seurat():
+	f = open('/home/ec2-user/filter-mode','w')
+	f.write('composition')
+	f.close()
+
+@route('/style/original')
+def seurat():
+	f = open('/home/ec2-user/filter-mode','w')
+	f.write('original')
+	f.close()
+
+run(host='0.0.0.0', port=8080, debug=True)
